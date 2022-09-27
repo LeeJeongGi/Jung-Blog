31d01be56ca3d9d0b9f89f481d64acf8b5650dfc
LeeJeongGi <apple4rhk@naver.com>
2022-09-27 23:01:53 +0900

    first commit

---
+++ src/main/java/com/junglog/JunglogApplication.java
@@ -0,0 +1,13 @@
+package com.junglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunglogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunglogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junglog/JunglogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junglog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunglogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
