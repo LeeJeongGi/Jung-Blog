c6f62d25aaaa9edd1a3aba2155c4933ee1b60719
LeeJeongGi <apple4rhk@naver.com>
2023-02-08 21:45:22 +0900

    refactor : 주석 제거

--- src/main/java/com/junglog/api/response/ErrorResponse.java
+++ src/main/java/com/junglog/api/response/ErrorResponse.java
@@ -6,15 +6,6 @@ import lombok.Getter;
 
 import java.util.Map;
 
-/**
- * {
- *     "code" : "400",
- *     "message" : "잘못된 요청입니다.",
- *     "validation" : {
- *          "title" : "값을 입력해주세요"
- *      }
- * }
- */
 @Getter
 @JsonInclude(value = JsonInclude.Include.NON_EMPTY) // 비어 있는 값은 내려가지 않도록 설정하는 코드
 public class ErrorResponse {
